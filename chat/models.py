f3689695e9b535ea3098e45ce0c8d5a425c21562
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-09-08 21:25:04 -0230

    Created a new model and some migrations

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 # Create your models here.
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    time = models.DateTimeField(default=now)
+    # ChatGPT response
+    response = models.TextField()
