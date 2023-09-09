8159f8780226f305baeba28f63484d1299794b9e
M=ArafatZaman <mdarafatzaman1@gmail.com>
2023-09-08 22:08:32 -0230

    A basic POST view for the register login view

--- chat/api.py
+++ chat/api.py
@@ -1,9 +1,24 @@
 from rest_framework import viewsets, views
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
+from django.contrib.auth.models import User
+from django.core.exceptions import FieldDoesNotExist
 
 class Register(views.APIView):
 
     def get(self, request):
         return JsonResponse({
             "username": "Arafat"
+        })
+    
+    def post(self, request: HttpRequest):
+        """
+        Create a new user
+        """
+        fields = ["first_name", "last_name", "email", "username", "password", "c_password"]
+        for f in fields:
+            if f not in request.POST:
+                raise FieldDoesNotExist(f"{f} is missing.")
+            
+        return JsonResponse({
+            "status": "Registered"
         })
