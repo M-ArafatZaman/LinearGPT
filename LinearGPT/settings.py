ecdd27cebc291bae4486735276cac17337b738bd
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-09-10 03:19:39 -0230

    Installed new accounts app which provides an interface to login, logout, and signup new users

--- LinearGPT/settings.py
+++ LinearGPT/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'knox',
-    'chat.apps.ChatConfig'
+    'chat.apps.ChatConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
