0169bebe6898e7b14beac8b0ca168237cc6eba8e
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-09-18 14:22:47 -0230

    Created a new parent chat model and added a foreign key to it from the ChatMessage model

--- chat/models.py
+++ chat/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=now)
+
 # Create your models here.
 class ChatMessage(models.Model):
+    '''
+    Either it is a ChatGPT response or a user chat message
+    '''
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     time = models.DateTimeField(default=now)
